fdb54d8e6fef75b01c870db59f30f6aa6a4e223d
Berlinda <56451493+BerlindaK@users.noreply.github.com>
2020-06-22 18:30:57 +0300

    Update BrowserFactory.java

--- first intellij project/src/com/cybertek/practice/BrowserFactory.java
+++ first intellij project/src/com/cybertek/practice/BrowserFactory.java
@@ -7,10 +7,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BrowserFactory {
     public static WebDriver getDriver(String browser){
         if (browser.equalsIgnoreCase("chrome")){
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vjendita.himaj\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Berlinda.Krasniqi\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
             return new ChromeDriver();
         }else if (browser.equalsIgnoreCase("firefox")){
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vjendita.himaj\\Documents\\selenium dependencies\\drivers\\geckoriver.exe");
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Berlinda.Krasniqi\\Documents\\selenium dependencies\\drivers\\geckoriver.exe");
             return new FirefoxDriver();
         }
         else {
